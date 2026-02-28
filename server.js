5bec25d6dd2395f05f4ea3ae54b3cd946ad3a8b0
Federico Milone <fedmilo91@gmail.com>
2026-02-28 00:45:46 -0300

    Change default port from 3000 to 3333

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 const N8N_WEBHOOK_URL = process.env.N8N_WEBHOOK_URL;
 
 app.use(express.static(path.join(__dirname, 'public')));
